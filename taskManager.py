66bc24945baa58d26fba381d2ec89ab78229c6e8
samathaluca <samanthaluca@aol.com>
2019-11-19 15:56:54 +0000

    mongoDB connection

--- taskManager.py
+++ taskManager.py
@@ -4,30 +4,30 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://rOOtUser:betteroption@myfirstcluster-97xkz.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_DBNAME"] = 'MS3_project'
+app.config["MONGO_URI"] = 'mongodb+srv://rOOtUser:betteroption@myfirstcluster-97xkz.mongodb.net/MS3_project?retryWrites=true&w=majority'
 #os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
 @app.route('/')
-@app.route('/get_tasks')
+@app.route('/get_persona')
 def get_tasks():
-    return render_template("tasks.html",
-                           tasks=mongo.db.tasks.find())
+    return render_template("persona.html",
+                           persona=mongo.db.persona.find())
 
 
-@app.route('/add_task')
-def add_task():
-    return render_template('addtask.html',
-                           categories=mongo.db.categories.find())
+# @app.route('/add_task')
+# def add_task():
+#     return render_template('addtask.html',
+#                            categories=mongo.db.categories.find())
 
 
-@app.route('/insert_task', methods=['POST'])
-def insert_task():
-    tasks = mongo.db.tasks
-    tasks.insert_one(request.form.to_dict())
-    return redirect(url_for('get_tasks'))
+# @app.route('/insert_task', methods=['POST'])
+# def insert_task():
+#     tasks = mongo.db.tasks
+#     tasks.insert_one(request.form.to_dict())
+#     return redirect(url_for('get_tasks'))
 
 
 if __name__ == '__main__':
