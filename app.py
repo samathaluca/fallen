e62d5c0289a33b66856f50edfcaf3f9e7c1e4a3f
samathaluca <samanthaluca@aol.com>
2019-11-20 16:05:02 +0000

    MongoDB connected, app.py running

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'MS3_project'
+app.config["MONGO_URI"] = 'mongodb+srv://rOOtUser:betteroption@myfirstcluster-97xkz.mongodb.net/MS3_project?retryWrites=true&w=majority'
+#os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return "ALL GOOD"
-
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("firstSteps.html", firstSteps=mongo.db.firstSteps.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
---
+++ app2.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "ALL GOOD"
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=(os.environ.get('PORT')),
+            debug=True)
+
+
