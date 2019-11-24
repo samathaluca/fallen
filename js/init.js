67c9a30c4baec0a3db36d840e298c6144af9a98f
samathaluca <samanthaluca@aol.com>
2019-11-24 18:32:34 +0000

    myProblem form tag repairs

--- js/init.js
+++ js/init.js
@@ -4,23 +4,21 @@ $(document).ready(function() {
         $('.collapsible').collapsible();
         $('select').formSelect();
         $('.sidenav').sidenav();
-        $('.datepicker').datepicker();
+        $(".datepicker").datepicker({
+            yearRange: 15,
+            autoClose: false,
+            showClearBtn: true,
+            i18n: {
+                clear: "Clear",
+                done: "Select",
+                cancel: "Cancel"
+            }
+        });
     }
     initMaterialize();
     
     document.getElementById("matfix").addEventListener("click", function(e) {
         e.stopPropagation();
     });
-
-
-    // $('.datepicker').datepicker();
-    // $('.datepicker').pickadate({
-    //     selectMonths: true, // Creates a dropdown to control month
-    //     selectYears: 15, // Creates a dropdown of 15 years to control year,
-    //     today: 'Today',
-    //     clear: 'Clear',
-    //     close: 'Ok',
-    //     closeOnSelect: false // Close upon selecting a date,
-    // });
 
 });
