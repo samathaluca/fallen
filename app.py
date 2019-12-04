2c38a67689dd1cf4ede7c33dc2aaaaa26b102b50
samathaluca <samanthaluca@aol.com>
2019-12-04 16:08:03 +0000

    images uploads

--- app.py
+++ app.py
@@ -46,13 +46,15 @@ def tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
-@app.route('/habits')
-def habits():
-    return render_template("habits.html", habit_manager=mongo.db.habit_manager.find())
+@app.route('/nineMinutes', methods=['GET', 'POST'])
+def nineMinutes():
+    if request.method == 'GET':
+        return render_template('nineMinutes.html', nine_minutes=mongo.db.nineMinutes.find())
+    else:
+        insert_nineMinutes = mongo.db.nineMinutes
+        insert_nineMinutes.insert_one(request.form.to_dict())
+        return redirect(url_for('index'))
 
-@app.route('/addHabits')
-def addHabits():
-    return render_template('addHabits.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',
