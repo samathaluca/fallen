ca552ce97c8a48ec21a01baa9ea35628ef706887
samathaluca <samanthaluca@aol.com>
2020-01-02 20:40:49 +0000

    deleted all old files and css beautifier

--- static/js/init.js
+++ static/js/init.js
@@ -6,11 +6,7 @@ $(document).ready(function() {
         $('.collapsible').collapsible();
         $('select').formSelect();
         $('.sidenav').sidenav();
-        
-        // $('.parallax').parallax();
-        
-
-    $('#alias').characterCounter();
+        $('#alias').characterCounter();
         // $(document).ready(function() {
         // M.updateTextFields();});
         // $('#textarea1').val('New Text');
@@ -26,35 +22,10 @@ $(document).ready(function() {
             }
         });
     }
-    initMaterialize();
+    // initMaterialize();
     
     // document.getElementById("matfix").addEventListener("click", function(e) {
     //     e.stopPropagation();
     // });
 
 });
-
-// $(document).ready(function() {
-
-//     function initMaterialize() {
-//         $('.collapsible').collapsible();
-//         $('select').formSelect();
-//         $('.sidenav').sidenav();
-//         $(".datepicker").datepicker({
-//             yearRange: 15,
-//             autoClose: false,
-//             showClearBtn: true,
-//             i18n: {
-//                 clear: "Clear",
-//                 done: "Select",
-//                 cancel: "Cancel"
-//             }
-//         });
-//     }
-//     initMaterialize();
-    
-//     document.getElementById("matfix").addEventListener("click", function(e) {
-//         e.stopPropagation();
-//     });
-
-// });
