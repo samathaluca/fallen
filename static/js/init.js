5853f14df4c921eea04b4dbf60f086853452a491
samathaluca <samanthaluca@aol.com>
2020-01-02 21:14:38 +0000

    select form repair

--- static/js/init.js
+++ static/js/init.js
@@ -6,8 +6,11 @@ $(document).ready(function() {
         $('.collapsible').collapsible();
         $('select').formSelect();
         $('.sidenav').sidenav();
-        $('#alias').characterCounter();
-        $('.carousel').carousel();
+        
+        // $('.parallax').parallax();
+        
+
+    $('#alias').characterCounter();
         // $(document).ready(function() {
         // M.updateTextFields();});
         // $('#textarea1').val('New Text');
@@ -23,10 +26,35 @@ $(document).ready(function() {
             }
         });
     }
-    // initMaterialize();
+    initMaterialize();
     
     // document.getElementById("matfix").addEventListener("click", function(e) {
     //     e.stopPropagation();
     // });
 
 });
+
+// $(document).ready(function() {
+
+//     function initMaterialize() {
+//         $('.collapsible').collapsible();
+//         $('select').formSelect();
+//         $('.sidenav').sidenav();
+//         $(".datepicker").datepicker({
+//             yearRange: 15,
+//             autoClose: false,
+//             showClearBtn: true,
+//             i18n: {
+//                 clear: "Clear",
+//                 done: "Select",
+//                 cancel: "Cancel"
+//             }
+//         });
+//     }
+//     initMaterialize();
+    
+//     document.getElementById("matfix").addEventListener("click", function(e) {
+//         e.stopPropagation();
+//     });
+
+// });
