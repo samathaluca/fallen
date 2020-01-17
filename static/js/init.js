81f7ff11167bdd9307660007d25932ddb3bb7a01
samathaluca <samanthaluca@aol.com>
2020-01-17 10:31:53 +0000

    html text changes, init.js and style.css cleanup

--- static/js/init.js
+++ static/js/init.js
@@ -1,14 +1,10 @@
 // This file includes the following necessary jquery to enable the Materialize javascript  included in the project to initialize.
 
-
-
 $(document).ready(function() {
-			// function initMaterialize() {
 				$('.collapsible').collapsible();
 				$('select').formSelect();
 				$('.sidenav').sidenav();
                 $('.modal').modal();
-                $('.tap-target').tapTarget();
 				$('#alias').characterCounter();
 				$(".datepicker").datepicker({
 					yearRange: 15,
@@ -19,32 +15,4 @@ $(document).ready(function() {
 						done: "Select",
 						cancel: "Cancel"}
                 });
-            // }
 });
-// 			}
-
-// $(document).ready(function() {
-
-//     function initMaterialize() {
-//         $('.collapsible').collapsible();
-//         $('select').formSelect();
-//         $('.sidenav').sidenav();
-//         $('.modal').modal();
-//         $('#alias').characterCounter();
-//         $(".datepicker").datepicker({
-//             yearRange: 15,
-//             autoClose: false,
-//             showClearBtn: true,
-//             i18n: {
-//                 clear: "Clear",
-//                 done: "Select",
-//                 cancel: "Cancel"
-//             }
-//         });
-    // }
-    // initMaterialize();
-    
-    // // document.getElementById("matfix").addEventListener("click", function(e) {
-    // //     e.stopPropagation();
-    // // });
-
